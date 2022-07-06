4656681f1a416370c313433e4cc37bace7bca926
BlitzCoding <cjswl1357@gmail.com>
2022-07-06 15:55:57 +0900

    array

--- Weak1/array.cpp
+++ Weak1/array.cpp
@@ -3,11 +3,18 @@
 |	[]연산자는 빠른 동작을 위해 전달된 인덱스 값이 배열의 크기보다 작은지 검사하진 않는다.
 |	-> at함수는 인자로 전달된 인덱스 값이 유효하지 않으면 std::out_of_range 발생
 |		-> 그러므로 느리지만 예외를 적절하게 활용할 수 있다.
+|	* 
+|	-단 array의 크기는 컴파일 시간에 결정되는 상우이어햐 한다. 즉 프로그램 실행중 변경 x
+|	-크기가 고정되어 있어서 원소를 추가하거나 삭제할 수 없다.
+|	-메모리 할당 방법을 변경할 수 없으므라 항상 스택 메모리를 사용한다. 
 -----------------------------------------------------------------*/
 
 #include <string>
 #include <algorithm>
 #include <iostream>
+
+#include <array>
+#include <type_traits>
 
 template <typename T>
 class dynamic_array
